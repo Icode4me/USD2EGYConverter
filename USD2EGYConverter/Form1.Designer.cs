bcf3eb7a75dae728bbedc4ce61a0f688c8525c78
Mohamed Hafez <mohamedhafez@outlook.com>
2020-04-25 23:49:53 -0400

    Add project files.

---
+++ USD2EGYConverter/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace USD2EGYConverter
+{
+    partial class USD2EGY_Converter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(USD2EGY_Converter));
+            this.lblCurrency1 = new System.Windows.Forms.Label();
+            this.lbeCurrency2 = new System.Windows.Forms.Label();
+            this.txtBoxCurrency1 = new System.Windows.Forms.TextBox();
+            this.txtBoxCurrency2 = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCurrency1
+            // 
+            resources.ApplyResources(this.lblCurrency1, "lblCurrency1");
+            this.lblCurrency1.Name = "lblCurrency1";
+            // 
+            // lbeCurrency2
+            // 
+            resources.ApplyResources(this.lbeCurrency2, "lbeCurrency2");
+            this.lbeCurrency2.Name = "lbeCurrency2";
+            // 
+            // txtBoxCurrency1
+            // 
+            resources.ApplyResources(this.txtBoxCurrency1, "txtBoxCurrency1");
+            this.txtBoxCurrency1.Name = "txtBoxCurrency1";
+            this.txtBoxCurrency1.TextChanged += new System.EventHandler(this.txtBoxCurrency1_TextChanged);
+            // 
+            // txtBoxCurrency2
+            // 
+            resources.ApplyResources(this.txtBoxCurrency2, "txtBoxCurrency2");
+            this.txtBoxCurrency2.Name = "txtBoxCurrency2";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnConvert, "btnConvert");
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // USD2EGY_Converter
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtBoxCurrency2);
+            this.Controls.Add(this.txtBoxCurrency1);
+            this.Controls.Add(this.lbeCurrency2);
+            this.Controls.Add(this.lblCurrency1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Name = "USD2EGY_Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCurrency1;
+        private System.Windows.Forms.Label lbeCurrency2;
+        private System.Windows.Forms.TextBox txtBoxCurrency1;
+        private System.Windows.Forms.TextBox txtBoxCurrency2;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ USD2EGYConverter/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+
+namespace USD2EGYConverter
+{
+    public partial class USD2EGY_Converter : Form
+    {
+        public USD2EGY_Converter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            Regex rg = new Regex("^[0-9]+$");
+
+            if (txtBoxCurrency1.Text == "")
+            {
+                MessageBox.Show("Please Enter a number in USD to Convert to Egyptian Pounds!");
+            }
+            else
+            {
+                if (rg.Match(txtBoxCurrency1.Text).Success)
+                {
+                    double usdCurrency1 = Convert.ToDouble(txtBoxCurrency1.Text);
+                    double multiplier = 15.75;
+                    double egyCurrency2 = Convert.ToDouble(usdCurrency1 * multiplier);
+                    txtBoxCurrency2.Text = egyCurrency2.ToString();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Only numbers allowed");
+                }
+            }    
+        }
+
+        private void txtBoxCurrency1_TextChanged(object sender, EventArgs e)
+        {
+            Regex rg = new Regex("^[0-9]+$");
+
+            if (rg.Match(txtBoxCurrency1.Text).Success && txtBoxCurrency1.Text != "")
+            {
+              
+            }
+            else
+            {
+                MessageBox.Show("Can't be blank & use only numerics please");
+            }
+
+        }
+    }
+}
